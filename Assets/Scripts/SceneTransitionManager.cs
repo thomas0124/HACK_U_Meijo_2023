94e730c1466e5654f4ca15f96e6d209c022221f5
M.R <m.r.MeijoUniv@gmail.com>
2023-11-21 15:30:24 +0900

    [add]タイトルシーンの追加

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance { get; private set; }
+    
+    [SerializeField] private Image transitionFadeImage; 
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SceneTransition(string sceneName)
+    {
+        transitionFadeImage.DOFade(1.0f, 2.0f).OnComplete(() => {
+            SceneManager.LoadScene(sceneName);
+            Debug.Log("SceneTransition");
+            transitionFadeImage.DOFade(0.0f, 2.0f);
+        });
+    }
+}
