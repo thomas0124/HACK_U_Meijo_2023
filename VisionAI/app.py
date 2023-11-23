ceb322e1f78d5aa3820efff04d87df7e6c7f6a41
トーマス <210441057@ccmailg.meijo-u.ac.jp>
2023-11-23 11:19:53 +0900

    feat:画像処理API,GitHub周りの設定

--- VisionAI/app.py
+++ VisionAI/app.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
 from typing import List
-import json
+import cv2
+import numpy as np
+import base64
+from fastapi.responses import JSONResponse
+from fastapi import UploadFile, File
+from io import BytesIO
 
 app = FastAPI()
 
-@app.post("/uploadfiles/")
-async def create_upload_files(files: List[UploadFile] = File(...)):
-    # 画像処理のコードをここに書く
-    # 得られた結果に応じて必殺技を割り振るコードをここに書く
-    name = "test"  # これは例です。実際のコードでは、画像処理の結果に基づいて必殺技を割り振る必要があります。
-    return {"name": name}
+@app.post("/image")
+async def post(image: UploadFile = File(...)):
+    """
+    画像をグレースケールに変換する
+    """
+    contents = await image.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    # 画像をグレースケールに変換
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # グレースケール画像をバイト列に変換
+    _, img_encoded = cv2.imencode('.png', gray_img)
+    img_bytes = img_encoded.tobytes()
+    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+    dataurl = 'data:image/png;base64,' + img_base64
+
+    return JSONResponse(content={'img_data': dataurl})
