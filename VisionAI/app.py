4dbf86b236e41bc17da6baf9e57f0002818d3a61
トーマス <210441057@ccmailg.meijo-u.ac.jp>
2023-11-24 22:04:41 +0900

    feat:test

--- VisionAI/app.py
+++ VisionAI/app.py
@@ -31,4 +31,4 @@ async def post(image: UploadFile = File(...)):
     return JSONResponse(content={'img_data': dataurl})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8883)
