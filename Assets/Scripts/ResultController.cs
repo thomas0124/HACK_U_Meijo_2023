501284abc1142237005889c1e364329b1d242a85
First-name Family-name <115214769+Atsuki210@users.noreply.github.com>
2023-12-14 17:18:10 +0900

    feat/シーン８ほぼ完成
    あとシーン２と繋げるだけ

--- Assets/Scripts/ResultController.cs
+++ Assets/Scripts/ResultController.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ResultController : MonoBehaviour
 {
-    public Text resultText;
+    public TextMeshProUGUI resultText;
+    public bool turn = true;
     
     void Start()
     {
-        resultText = resultText.GetComponent<Text>();//テキストコンポーネントを取得
-
         if(BattleController.turn)
         {
+            resultText.color = new Color(1.0f, 0.8f, 0.0f, 1.0f);
             resultText.text = "YOU WIN!";//自分が勝った時
         }
         else{
+            resultText.color = new Color(0.1f, 0.0f, 1.0f, 1.0f);
             resultText.text = "TOU LOSE";//自分が勝った時
         }
     }
